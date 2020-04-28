fb6b6690028c68b77e894fd78a4a8cfa80287438
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 11:32:10 +0100

    Updates to PEP 8

--- src/exercise.py
+++ src/exercise.py
@@ -1,7 +1,7 @@
 def main():
-  #write your code below this line
-  message = input("Write a message...")
-  print(message)
+    #write your code below this line
+    message = input("Write a message...")
+    print(message)
 
 if __name__ == '__main__':
     main()
